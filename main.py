ab53121c6c3e50cd41038650de5adb17f29d4fe2
saidali122 <nuquljonjalilova@gmail.com>
2025-02-14 18:16:35 +0500

    paython_ism

---
+++ main.py
@@ -0,0 +1,10 @@
+# def user_data(first_name, last_name, age):
+#     print(f"Ism: {first_name}")
+#     print(f"Familiya: {last_name}")
+#     print(f"Yosh: {age}")
+#
+# first_name = input("ismingizni kiriting>> ")
+# last_name = input("familyangizni kiriting>> ")
+# age = int(input("yoshizni kiriting>> "))
+#
+# user_data(first_name, last_name, age)
